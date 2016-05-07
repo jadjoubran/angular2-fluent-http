2588e7e295c3bdf4c05578607e38a6f467c4c834
Jad Joubran <joubran.jad@gmail.com>
2016-05-07 19:26:18 +0300

    baseUrl support

--- src/services/fluent.service.ts
+++ src/services/fluent.service.ts
@@ -1,9 +1,64 @@
 import {Injectable} from '@angular/core';
+import {Http, RequestOptionsArgs, Response} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class FluentService{
+	private baseUrl: string;
 
-  constructor() {
-    
-  }
+	constructor(private http: Http) {
+
+	}
+
+	setBaseUrl(baseUrl: string){
+		if ( baseUrl && baseUrl.substr(-1) !== '/'){
+			baseUrl = baseUrl + '/';
+		}
+		this.baseUrl = baseUrl;
+	}
+
+	getBaseUrl(){
+		return this.baseUrl;
+	}
+
+	private getUrl(endpoint: string){
+		return this.baseUrl + endpoint;
+	}
+
+	get(endpoint: string, options?: RequestOptionsArgs) : Observable<Response>{
+		let url = this.getUrl(endpoint);
+
+		return this.http.get(url, options);
+	}
+
+	post(endpoint: string, body: string, options?: RequestOptionsArgs) : Observable<Response>{
+		let url = this.getUrl(endpoint);
+
+		return this.http.post(url, body, options);
+	}
+
+	put(endpoint: string, body: string, options?: RequestOptionsArgs) : Observable<Response>{
+		let url = this.getUrl(endpoint);
+
+		return this.http.put(url, body, options);
+	}
+
+	delete(endpoint: string, options?: RequestOptionsArgs) : Observable<Response>{
+		let url = this.getUrl(endpoint);
+
+		return this.http.delete(url, options);
+	}
+
+	patch(endpoint: string, body: string, options?: RequestOptionsArgs) : Observable<Response>{
+		let url = this.getUrl(endpoint);
+
+		return this.http.patch(url, body, options);
+	}
+
+	head(endpoint: string, options?: RequestOptionsArgs) : Observable<Response>{
+		let url = this.getUrl(endpoint);
+
+		return this.http.head(url, options);
+	}
+
 }
