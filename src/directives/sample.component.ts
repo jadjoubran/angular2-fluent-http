1c938d1aa180d57e3ed6d47b9f0a911db17720b1
Jad Joubran <joubran.jad@gmail.com>
2016-05-07 19:04:35 +0300

    upgraded to angular2 rc

--- src/directives/sample.component.ts
+++ src/directives/sample.component.ts
@@ -1,15 +1,15 @@
-import {Component} from 'angular2/core';
+// import {Component} from 'angular2/core';
 
-@Component({
-  selector: 'sample-component',
-  template: `
-    Sample component
-  `
-})
-export class SampleComponent {
+// @Component({
+  // selector: 'sample-component',
+  // template: `
+    // Sample component
+  // `
+// })
+// export class SampleComponent {
 
-  constructor(){
+  // constructor(){
 
-  }
+  // }
 
-}
+// }
